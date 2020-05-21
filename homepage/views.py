98f0085292797583593c9052bdcbc9ed691f2621
Ernir <e.ernir@gmail.com>
2020-05-21 16:51:49 +0200

    Include an index page

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class BaseView(View):
+    """
+    An "abstract view" to manage the elements all of the site's pages have in common
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.params = {
+            "title_suffix": "",
+            "debug": settings.DEBUG,
+        }
+
+
+class IndexView(BaseView):
+    """
+    A view for the site's index page
+    """
+
+    def get(self, request):
+        return render(request, "index.html", self.params)
