e23a1662ea94a1330bbf21d465b0669ac88d8e48
Ernir <e.ernir@gmail.com>
2020-05-30 17:57:21 +0200

    Read photos from a dedicated subfolder

--- photos/management/commands/read_photos.py
+++ photos/management/commands/read_photos.py
@@ -10,15 +10,17 @@ from photos.models import Photo, Gallery
 
 
 class Command(BaseCommand):
+
     def __init__(self):
         super().__init__()
+        self.galleries_prefix = "/galleries"
         self.dbx = dropbox.Dropbox(os.environ.get("DROPBOX_OAUTH2_TOKEN"))
 
     def get_folders(self) -> List[str]:
         """
-        :return the names of folders in this application's root Dropbox directory.
+        :return the names of folders in this application's gallery Dropbox directory, see self.galleries_prefix.
         """
-        for f in self.dbx.files_list_folder("").entries:
+        for f in self.dbx.files_list_folder(self.galleries_prefix).entries:
             if isinstance(f, FolderMetadata):
                 yield f.name
 
@@ -27,31 +29,33 @@ class Command(BaseCommand):
         :param folder_name: the name of a Dropbox folder
         :return the names of files in the given Dropbox folder
         """
-        path = f"/{folder_name}"
+        path = f"{self.galleries_prefix}/{folder_name}"
         for f in self.dbx.files_list_folder(path).entries:
             if isinstance(f, FileMetadata):
                 yield f.name
 
+    def instantiate_from_folder(self, folder):
+        gallery, created = Gallery.objects.get_or_create(identifier=folder)
+        if created:
+            gallery.name = gallery.identifier
+            gallery.save()
+        existing_photos = {photo.image for photo in gallery.photo_set.all()}
+        files_on_dropbox = {
+            f"{self.galleries_prefix}/{folder}/{filename}" for filename in self.get_files_of_folder(folder)
+        }
+        added = files_on_dropbox.difference(existing_photos)
+        for path in added:
+            print(f"Adding {path}")
+            photo = Photo()
+            photo.image = path
+            photo.description = path
+            photo.gallery = gallery
+            photo.save()
+        removed = existing_photos.difference(files_on_dropbox)
+        for photo in gallery.photo_set.filter(image__in=removed).all():
+            print(f"Removing {photo}")
+            photo.delete()
+
     def handle(self, *args, **options) -> None:
         for folder in self.get_folders():
-            gallery, created = Gallery.objects.get_or_create(identifier=folder)
-            if created:
-                gallery.name = gallery.identifier
-                gallery.save()
-
-            existing_photos = {photo.image for photo in gallery.photo_set.all()}
-            files_on_dropbox = {
-                f"/{folder}/{filename}" for filename in self.get_files_of_folder(folder)
-            }
-            added = files_on_dropbox.difference(existing_photos)
-            for path in added:
-                print(f"Adding {path}")
-                photo = Photo()
-                photo.image = path
-                photo.description = path
-                photo.gallery = gallery
-                photo.save()
-            removed = existing_photos.difference(files_on_dropbox)
-            for photo in gallery.photo_set.filter(image__in=removed).all():
-                print(f"Removing {photo}")
-                photo.delete()
+            self.instantiate_from_folder(folder)
--- photos/models.py
+++ photos/models.py
@@ -14,6 +14,9 @@ class Gallery(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Galleries"
+
 
 class Photo(models.Model):
     """
